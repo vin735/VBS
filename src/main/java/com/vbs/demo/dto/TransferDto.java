510d97ba58808b057bdea3d346783dbf6a0ea7c2
vin735 <suryawanshivineet32@gmail.com>
2025-12-30 18:55:14 +0530

    First Commit

---
+++ src/main/java/com/vbs/demo/dto/TransferDto.java
@@ -0,0 +1,16 @@
+package com.vbs.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class TransferDto {
+    String username;
+    int id;
+    double amount;
+
+}
